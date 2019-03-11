6293e66f5ee674884d42f3286eb09914600359c7
Hao Nguyen <nguyenphuhao.it@gmail.com>
2019-03-11 13:19:23 +0700

    Updates

---
+++ precache-manifest.79ee9b0577b3fd598d0c020cad51c670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marvel-manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "73459ffb2bf72f74029d",
+    "url": "/marvel-manager/static/js/runtime~main.2656a4bb.js"
+  },
+  {
+    "revision": "d1173a7d1cbe0d40caa0",
+    "url": "/marvel-manager/static/js/main.1e07c08f.chunk.js"
+  },
+  {
+    "revision": "3b9455b57f78495b5513",
+    "url": "/marvel-manager/static/js/2.c172abb4.chunk.js"
+  },
+  {
+    "revision": "d1173a7d1cbe0d40caa0",
+    "url": "/marvel-manager/static/css/main.207fe9a3.chunk.css"
+  },
+  {
+    "revision": "3b9455b57f78495b5513",
+    "url": "/marvel-manager/static/css/2.b883880c.chunk.css"
+  },
+  {
+    "revision": "d3e9f33e24ad2d6d7b57770d17a34b61",
+    "url": "/marvel-manager/index.html"
+  }
+];
