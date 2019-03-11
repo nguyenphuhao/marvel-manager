c21705c70ec278323fad000ebd66dcef6e93c264
Hao Nguyen <nguyenphuhao.it@gmail.com>
2019-03-11 16:52:31 +0700

    Update URL

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import '@progress/kendo-theme-material/dist/all.css';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import CharacterDetails from './components/marvel/characters/CharacterDetails';
-
+import CharacterList from './components/marvel/characters/CharacterList';
 class App extends Component {
 
   render() {
+    const uri = "marvel-manager";
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/character/:id" component={CharacterDetails} />
+            <Route exact path={uri + "/"} component={Dashboard} />
+            <Route path={uri + "/character"} component={CharacterList} />
+            <Route path={uri + "/character/:id"} component={CharacterDetails} />
           </Switch>
         </div>
       </BrowserRouter>
