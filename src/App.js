4d19616da6d3655c7b1723f04bd084bed8f9f979
Hao Nguyen <nguyenphuhao.it@gmail.com>
2019-03-11 11:18:03 +0700

    Initialize the project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '@progress/kendo-theme-bootstrap/dist/all.css';
+import './App.css';
+
+// Import the Grid component.
+import { Grid, GridColumn } from '@progress/kendo-react-grid';  
+
+class App extends Component {
+ constructor(props) {
+   super(props);
+
+   this.state = {
+     data: [
+       {ProductName: "Chai", Price: 10},
+       {ProductName: "Chang", Price: 20}
+     ],
+     title: "Hello from KendoReact!"
+   }
+ }
+ render() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <h1 className="App-title">{this.state.title}</h1>
+       </header>
+         <Grid data={this.state.data}>
+           <GridColumn field="ProductName" title="Product name"/>
+           <GridColumn field="Price"/>
+         </Grid>
+     </div>
+   );
+ }
+}
+
+export default App;
